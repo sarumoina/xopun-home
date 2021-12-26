3e13d21d6be997384ab1963cb8e6bf1d70322207
sarumoina <sarumoina.bak@gmail.com>
2021-12-26 12:50:08 +0530

    m1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,14 +4,15 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;   
 
-class HomeController {
+class HomeController extends AbstractController {
 
     /**
      * @Route("/")
      */
 
     public function index() : Response {
-        return new Response('1');
+        return $this->render('home/index.html.twig');
     }
 }
